379165c3a5237c7116e40226d51597f0f5c595cd
Aamir Hussain <aamir222686@yahoo.com>
2023-03-18 22:50:27 +0530

    dep check

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,33 +32,37 @@ export default function Home(props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container maxWidth="xl" sx={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "20px" }}>
-        <Typography variant='h3' padding={5}>Audio Summariser</Typography>
-        <Box sx={{width: "80%", display: "flex", justifyContent: "space-around", marginBottom: "20px", color: "lightblue"}}>
-          <Typography variant='body2' padding={1}><b>Email:</b> aamir222686@yahoo.com</Typography>
-          <Typography variant='body2' padding={1}><b>Git:</b> <a href='https://github.com/aamir222686' target={'_blank'}>@aamir222686</a></Typography>
-        </Box>
-        <Box>
-          <Typography variant='body1'>
-            <em>This is a proof of concept which contains a pipeline in the backend which does the following</em>
-          </Typography><br></br>
-          <ul style={{marginLeft: "20px", fontFamily: "roboto", lineHeight: "35px"}}>
-              <li>Uses the input audio file/s and generates the Transcript for it using OpenAi's Whisper Model.</li>
-              <li>Next the pipeline generates the Short Summary and a Short Explaination using GPT's "text-davinci-003" model.</li>
-              <li>The file is then saved to firebase and the generated data is saved to a MongoDB collection</li>
-              <li>The UI below showcases the generative capabilities and some example ways to handle these transcripts for the input audio files.</li>
-          </ul>
-        </Box>
-        <Container sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", padding: "20px" }}>
-          {
-            dataArray.map((item, i) =>
-              {
-                return <ViewBlock blockData={item} key={i+1} num={i} />
-              }
-            )
-          }
+      {dataArray && dataArray.length > 0 ?
+        <Container maxWidth="xl" sx={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "20px" }}>
+          <Typography variant='h3' padding={5}>Audio Summariser</Typography>
+          <Box sx={{width: "80%", display: "flex", justifyContent: "space-around", marginBottom: "20px", color: "lightblue"}}>
+            <Typography variant='body2' padding={1}><b>Email:</b> aamir222686@yahoo.com</Typography>
+            <Typography variant='body2' padding={1}><b>Git:</b> <a href='https://github.com/aamir222686' target={'_blank'}>@aamir222686</a></Typography>
+          </Box>
+          <Box>
+            <Typography variant='body1'>
+              <em>This is a proof of concept which contains a pipeline in the backend which does the following</em>
+            </Typography><br></br>
+            <ul style={{marginLeft: "20px", fontFamily: "roboto", lineHeight: "35px"}}>
+                <li>Uses the input audio file/s and generates the Transcript for it using OpenAi's Whisper Model.</li>
+                <li>Next the pipeline generates the Short Summary and a Short Explaination using GPT's "text-davinci-003" model.</li>
+                <li>The file is then saved to firebase and the generated data is saved to a MongoDB collection</li>
+                <li>The UI below showcases the generative capabilities and some example ways to handle these transcripts for the input audio files.</li>
+            </ul>
+          </Box>
+          <Container sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", padding: "20px" }}>
+            {
+              dataArray.map((item, i) =>
+                {
+                  return <ViewBlock blockData={item} key={i+1} num={i} />
+                }
+              )
+            }
+          </Container>
         </Container>
-      </Container>
+        :
+        null
+      }
     </>
   )
 }
